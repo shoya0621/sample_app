0cb6583d598a702b6d5748406d5c249239200f50
shoya0621 <122379046+shoya0621@users.noreply.github.com>
2023-02-02 14:39:57 +0000

    7章

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get '/top' =>'homes#top'
   post 'lists'=>'lists#create'
   patch 'lists/:id' => 'lists#update', as:'update_list'
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
